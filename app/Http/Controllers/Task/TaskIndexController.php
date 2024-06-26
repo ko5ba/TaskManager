5534e5391cbbdc085b82bd5c9c610f9846964cb2
ko5ba <104355972+ko5ba@users.noreply.github.com>
2024-06-26 21:04:39 +0300

    feat: creating a task model and migrate

---
+++ app/Http/Controllers/Task/TaskIndexController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Task;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TaskIndexController extends Controller
+{
+    public function __invoke()
+    {
+        
+    }
+}
