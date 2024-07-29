c5994f8e0fddae5defefbc4280a110d9bc84e099
ko5ba <104355972+ko5ba@users.noreply.github.com>
2024-07-29 14:44:54 +0300

    fix: routes

---
+++ app/Http/Controllers/Task/TaskController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\Task;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Task\StoreRequest;
+use App\Http\Requests\Task\UpdateRequest;
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $tasks = Task::all();
+        return inertia('Task/Index', compact('tasks'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return inertia('Task/Create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreRequest $request, Task $task)
+    {
+        $task->create($request->validated());
+        return to_route('tasks.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Task $task)
+    {
+        return inertia('Task/Show', compact('task'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Task $task)
+    {
+        return inertia('Task/Edit', compact('task'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateRequest $request, Task $task)
+    {
+        $task->update($request->validated());
+        return to_route('tasks.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Task $task)
+    {
+        $task->delete();
+        return redirect()->route('tasks.index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Task\TaskController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -22,20 +23,23 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/tasks', App\Http\Controllers\Task\TaskIndexController::class)->name('tasks.index');
-    Route::get('/task_create', App\Http\Controllers\Task\TaskCreateController::class)->name('tasks.create');
-    Route::post('/tasks', App\Http\Controllers\Task\TaskStoreController::class)->name('tasks.store');
-    Route::get('/tasks/{task}', App\Http\Controllers\Task\TaskShowController::class)->name('tasks.show');
-    Route::get('/tasks/{task}/edit', App\Http\Controllers\Task\TaskEditController::class)->name('tasks.edit');
-    Route::patch('/tasks/{task}', App\Http\Controllers\Task\TaskUpdateController::class)->name('tasks.update');
-    Route::delete('/tasks/{task}', App\Http\Controllers\Task\TaskDestroyController::class)->name('tasks.destroy');
-    Route::get('/tags', App\Http\Controllers\Tag\TagIndexController::class)->name('tags.index');
-    Route::get('/tag_create', App\Http\Controllers\Tag\TagCreateController::class)->name('tags.create');
-    Route::post('/tags', App\Http\Controllers\Tag\TagStoreController::class)->name('tags.store');
-    Route::get('/tags/{tag}', App\Http\Controllers\Tag\TagShowController::class)->name('tags.show');
-    Route::get('/tags/{tag}/edit', App\Http\Controllers\Tag\TagEditController::class)->name('tags.edit');
-    Route::patch('/tags/{tag}', App\Http\Controllers\Tag\TagUpdateController::class)->name('tags.update');
-    Route::delete('/tags/{tag}', App\Http\Controllers\Tag\TagDestroyController::class)->name('tags.destroy');
+    Route::resource('/tasks', TaskController::class)
+        ->missing(function() {
+            return redirect()->route('tasks.index');
+        });
 });
+
+/*
+*Добавить после обновления таблицы tags
+*Route::get('/tags', App\Http\Controllers\Tag\TagIndexController::class)->name('tags.index');
+*Route::get('/tag_create', App\Http\Controllers\Tag\TagCreateController::class)->name('tags.create');
+*Route::post('/tags', App\Http\Controllers\Tag\TagStoreController::class)->name('tags.store');
+*Route::get('/tags/{tag}', App\Http\Controllers\Tag\TagShowController::class)->name('tags.show');
+*Route::get('/tags/{tag}/edit', App\Http\Controllers\Tag\TagEditController::class)->name('tags.edit');
+*Route::patch('/tags/{tag}', App\Http\Controllers\Tag\TagUpdateController::class)->name('tags.update');
+*Route::delete('/tags/{tag}', App\Http\Controllers\Tag\TagDestroyController::class)->name('tags.destroy');
+});
+*/
+
 
 require __DIR__.'/auth.php';
